a203814c6d1695e51d15b965142d618eb14cf67e
Facundo Sergio <46017478+facu-sergio@users.noreply.github.com>
2026-02-18 14:54:47 -0300

    add home page with list cards and header nav link (#8)

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { ListDetailComponent } from './components/list-detail/list-detail';
+import { HomeComponent } from './components/home/home';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'juegos/:id',
     component: ListDetailComponent
