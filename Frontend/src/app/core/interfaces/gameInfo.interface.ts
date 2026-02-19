177493bfde845c64f966b29734ef95997c0abbb0
Facundo Sergio <46017478+facu-sergio@users.noreply.github.com>
2026-02-19 16:22:47 -0300

    Status change from completed view (#13)
    * add status change support to completed games view and generalize dialog

--- Frontend/src/app/core/interfaces/gameInfo.interface.ts
+++ Frontend/src/app/core/interfaces/gameInfo.interface.ts
@@ -3,6 +3,7 @@ import { GameStatus } from './game-status.type';
 export interface GameInfo {
   id?: number;
   igdbId: number;
+  userListId?: number;
   name: string;
   coverUrl?: string;
   firstReleaseDate?: number;
