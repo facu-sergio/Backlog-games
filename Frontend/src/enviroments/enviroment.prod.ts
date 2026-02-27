4ae85b195e80e4e6a50e839ad9e2e820871f4b1c
facu sergio <facu11ja@gmail.com>
2026-02-26 23:56:32 -0300

    deplot front

--- Frontend/src/enviroments/enviroment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://game-backlog-api.onrender.com/api'
-};
--- Frontend/src/enviroments/enviroment.ts
+++
@@ -1,6 +0,0 @@
-// environment.ts (Development)
-export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7095/api/v1',
-  appName: 'Game Backlog - DEV' 
-};
